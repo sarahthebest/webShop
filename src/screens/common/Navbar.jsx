be732597e1e9931a1c98c65830b4a78010411a1b
Sarah <sarah.emmoth@gmail.com>
2024-05-02 17:33:54 +0200

    font and logo edit

--- src/screens/common/Navbar.jsx
+++ src/screens/common/Navbar.jsx
@@ -4,14 +4,17 @@ const Navbar = () => {
       <div className="nav grid-cols-6 grid-rows-2 justify-center bg-slate-100 sticky top-0">
         <div className="row flex justify-center">
           <img
-            src="src\assets\img\logo2.png"
+            src="src\assets\img\logo.webp"
             alt="logo"
             id="logo"
-            className="my-4"
+            className="my-2"
           />
         </div>
         <div className="row flex">
-          <ul className="links list-none flex flex-row gap-4 justify-center items-center border-2">
+          <ul className="links list-none flex flex-row gap-5 justify-center items-center border-2">
+          <li className="hover:scale-110">
+              <a href="">Bestsellers</a>
+            </li>
             <li className="hover:scale-110">
               <a href="">Rings</a>
             </li>
@@ -20,6 +23,9 @@ const Navbar = () => {
             </li>
             <li className="hover:scale-110">
               <a href="">Bracelets</a>
+            </li>
+            <li className="hover:scale-110">
+              <a href="">Earrings</a>
             </li>
           </ul>
         </div>
